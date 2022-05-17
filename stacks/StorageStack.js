1242b12281cbbe82ae33e03e4d86d64b6ccee3ec
Min Park <miso419@gmail.com>
2022-05-17 10:07:50 +0900

    Add a storge stack

---
+++ stacks/StorageStack.js
@@ -0,0 +1,25 @@
+import * as sst from '@serverless-stack/resources';
+
+export default class StorageStack extends sst.Stack {
+  table;
+  bucket;
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    // Create a DynamoDB
+    this.table = new sst.Table(this, 'Notes', {
+      fields: {
+        userId: sst.TableFieldType.STRING,
+        noteId: sst.TableFieldType.STRING,
+      },
+      primaryIndex: {
+        partitionKey: 'userId',
+        sortKey: 'noteId',
+      },
+    });
+
+    // Create a S3 bucket
+    this.bucket = new sst.Bucket(this, 'Uploads');
+  }
+}
